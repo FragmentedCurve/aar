39c07641409c7e9ad0e0507ca704629a3fa06a0b
Paco Pascal <me@pacopascal.com>
2024-07-20 13:51:17 -0400

    Begin support for building on different platforms

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 PROG=aar
 SRCS=build.c contrib/aes256/aes256.c
-CFLAGS+=-std=c99 -pedantic
+CFLAGS+=-std=c99 -pedantic -D AAR_OS_POSIX
 .ifdef DEBUG
 CFLAGS+= -O0 -ggdb -pg -D _AAR_DEBUG_NOCRYPT 
 .endif
--- build.c
+++ build.c
@@ -1,4 +1,6 @@
+#ifdef AAR_OS_POSIX
 #define _XOPEN_SOURCE 600
+#endif
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -7,15 +9,19 @@
 #include <assert.h>
 
 #include "libs/typeok.h"
+#include "libs/base64.h"
+
 #define NSTRINGS_MAIN
 #include "libs/nstrings.h"
-#include "libs/base64.h"
 
 #define BACK_TO_TABLES // Use pre-calculated tables for AES
 #include "contrib/aes256/aes256.h"
 
 #include "aarfmt.h"
 
+#ifdef AAR_OS_POSIX
 #include "os_posix.c"
+#endif
+
 #include "diskops.c"
 #include "main.c"
