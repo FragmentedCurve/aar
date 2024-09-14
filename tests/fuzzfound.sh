326dd3d42c978b0ad47d32d69a0efc6bc3577326
Paco Pascal <me@pacopascal.com>
2024-09-14 00:02:34 -0400

    Add a test for the bugs found via fuzzing
    These should never appear again anyway.

---
+++ tests/fuzzfound.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Bugs in base64 found by radamsa
+
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=cprfiedy -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=rcdlpiyt -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=ipelcrey -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=elycetrf -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=eflcdyir -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=flcyipet -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=treepicy -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=tyflpcir -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=yteierfc -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=piltceye -a ${TEST}.tmp new
