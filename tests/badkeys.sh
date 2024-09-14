81d003844b195d8e6109947bcd07cab12ad4e0d5
Paco Pascal <me@pacopascal.com>
2024-09-14 08:04:47 -0400

    Include more invalid keys

--- tests/badkeys.sh
+++ tests/badkeys.sh
@@ -4,11 +4,8 @@
 
 ! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=cprfiedy -a ${TEST}.tmp new
 ! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=rcdlpiyt -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=ipelcrey -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=elycetrf -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=eflcdyir -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=flcyipet -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=treepicy -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=tyflpcir -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=yteierfc -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=piltceye -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJa=====         -a ${TEST}.tmp new
+! ${AAR} -k AA==                                                 -a ${TEST}.tmp new
+! ${AAR} -k AAAA                                                 -a ${TEST}.tmp new
+! ${AAR} -k AAAAzzz=                                             -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Yz         -a ${TEST}.tmp new
