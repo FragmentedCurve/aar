f5cadcc5941fc5fc9a00a61f1e42b1b0a7982c44
Paco Pascal <me@pacopascal.com>
2024-09-16 15:34:47 -0400

    Corrected the accuracy of a comment

--- tests/badkeys.sh
+++ tests/badkeys.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Bugs in base64 found by radamsa
-
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=cprfiedy -a ${TEST}.tmp new
-! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=rcdlpiyt -a ${TEST}.tmp new
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=cprfiedy -a ${TEST}.tmp new # Found by radamsa
+! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Y=rcdlpiyt -a ${TEST}.tmp new # Found by radamsa
 ! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJa=====         -a ${TEST}.tmp new
 ! ${AAR} -k AA==                                                 -a ${TEST}.tmp new
 ! ${AAR} -k AAAA                                                 -a ${TEST}.tmp new
 ! ${AAR} -k AAAAzzz=                                             -a ${TEST}.tmp new
 ! ${AAR} -k uNUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Yz         -a ${TEST}.tmp new
+! ${AAR} -k =NUvs/VIKgEW6jAIJ3DOUWYuZUtFiUFDk1ZWRJaRF4Yz         -a ${TEST}.tmp new
